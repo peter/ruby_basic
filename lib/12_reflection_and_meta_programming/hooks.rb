bb00ace2e81b960c1f0033895e0febfb69eb150b
Peter Marklund <peter@marklunds.com>
2011-09-01 13:20:30 +0200

    Fixed broken hooks.rb code example

--- lib/12_reflection_and_meta_programming/hooks.rb
+++ lib/12_reflection_and_meta_programming/hooks.rb
@@ -1,12 +1,12 @@
 # Adds the current directory to the Ruby load path
-# The reason we do this here is so that we can Ruby will find the Person
+# The reason we do this here is so that we Ruby will find the Person
 # class when we require it. You can invoke ruby on the command line with the I option
 # to add directories to the load path.
-$:.unshift(File.dirname(__FILE__))
+$:.unshift(File.dirname(__FILE__) + '/../03_classes_and_objects')
 
 # Makes sure a class is required automatically whenever it is used.
 # This is a naive implementation.
-def Object.const_missing(name) 
+def Object.const_missing(name)
   @looked_for ||= {} 
   str_name = name.to_s 
   raise "Class not found: #{name}" if @looked_for[str_name] 
@@ -18,5 +18,6 @@ def Object.const_missing(name)
   raise "Class not found: #{name}" 
 end
 
+# With const_missing in place we can now use our Person class without first doing a require 'person'
 person = Person.new("Peter")
 person.say_hi
